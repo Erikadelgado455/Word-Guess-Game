d5e3d25ac556e2acd45737aa907e4dea93e0e3b6
erikadelgado455 <erikadelgado455@yahoo.com>
2018-07-29 04:57:14 -0400

    i added an alphabet array and a fuction that shows the chosen letter to display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,57 @@
 
-    var wordarray =["jeep", "mazda", "toyota", "honda", "ford", "chevorlet", "hyundai"];
-    var randomword=wordarray[Math.floor(Math.random()*wordarray.length)];
-    var randomwordsplit=randomword.split('');
+    var wordbank =["jeep", "mazda", "toyota", "honda", "ford", "chevorlet", "hyundai"];
+    var words=wordbank[Math.floor(Math.random()*wordbank.length)];
+    var wordsplit=words.split('');
     
     var x;
-    var count = 0;
-    var answerarray=[];
-    function startup()
+    var timesguessed = 0;
+    var answer=[];
+    var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var chosen=[]
+    function game()
     {
-        for(var i=0; i< randomword.length; i++)
+        for(var i=0; i< words.length; i++)
         {
-            answerarray[i]="_ ";
+            answer[i]="_ ";
+            
         }
-        s=answerarray.join(" ");
+        x=answer.join(" ");
         document.getElementById("answer").innerHTML.x;
-    }
-    function Letter() 
-    {
-        var letter=document.getElementById("letter").value;
-        if(letter.length>0)
+        for(var i=0; i< alphabet.length; i++)
         {
-            for(var i=0; i<randomword.length; i++)
+            chosen[i]="$ ";
+        }
+        y=chosen.join(" ");
+        document.getElementById("chosen").innerHTML.y;
+    }
+    function Letterguessed() 
+    {
+        var letterguessed=document.getElementById("letterguessed").value;
+        if(letterguessed.length>0)
+        {
+            for(var i=0; i<words.length; i++)
             {
-                if(randomword[i]===letter)
+                if(words[i]===letterguessed)
                 {
-                    answerarray[i]=letter;
+                    answer[i]=letterguessed;
+                }
+                else{
+                    
                 }
             }
-            count++;
-            document.getElementById("timesguessed").innerHTML="Times Guessed:"+count;
-            document.getElementById("answer").innerHTML=answerarray.join(" ");
+            for(var i=0; i<alphabet.length; i++)
+            {
+                if(alphabet[i]===letterguessed)
+                {
+                    chosen[i]=letterguessed;
+                }
+            }
+           
+            timesguessed++;
+            document.getElementById("timesguessed").innerHTML="Times Guessed:"+timesguessed;
+            document.getElementById("answer").innerHTML=answer.join(" ");
+            document.getElementById("chosen").innerHTML=chosen.join(" ");
         }
-        
     }
 
    
